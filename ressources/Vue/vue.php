dbc3e9efa3fab596ca922d2c2d65f63bbb9e3dd1
thoma <thoma@Thomas>
2017-12-05 15:21:21 +0100

    Images changées par des cercles avec CSS et fond gris

--- ressources/Vue/vue.php
+++ ressources/Vue/vue.php
@@ -3,34 +3,38 @@
 class Vue {
 
     function demandePseudo() {
-        echo '<form class="login-form"method="POST" action="." >
+        echo '<form class="login-form" method="POST" action="." >
                 <input type="text" placeholder="Nom d\'utilisateur" name="username"/>
                 <input type="password" placeholder="Mot de passe" name="password" />
                 <button type="submit">Connexion</button>
               </form>';
+        echo '<div class="circle"></div>';
     }
 
     function affichagePlateau() {
-        $html = '<head><link rel="stylesheet" type="text/css" href="css.css"></head>';
-        $html .= "\n <html> \n";
-        $html .= "\t <table border> \n";
+        $html = "<html> \n";
+        $html .= '<head><link rel="stylesheet" type="text/css" href="css.css"></head>';
+        $html .= '<body bgcolor="#F5F5F5">';
+        $html .= "\n \t <table> \n";
         for ($i = 0; $i < 7; $i++) { // lignes du plateau
             $html .= "\t \t <tr> \n";
             for ($j = 0; $j < 7; $j++) { // colonnes du plateau
                 $html .= "\t \t \t <td>";
-                $html .= '<button class="classe" type="submit" name="bille">';
+                $html .= '<form action="." method="post"><button class="classe" type="submit" name="bille" value="' . $i . ';' . $j . '"></form>';
                 if ($i > 1 && $i < 5) { // remplissage des lignes 2 à 4
-                    $html .= '<img src="../image/bille.png" width="40" name="bille" />';
+                    $html .= '<div class="circleblack"></div>';
                 } elseif ($i < 2 || $i > 4 && $i < 7) { // remplissage des lignes 0,1,5 et 6
                     if ($j > 1 && $j < 5) { // seulement les cases 2 à 4 sont remplis sur ces lignes
-                        $html .= '<img src="../image/bille.png" width="40" name="bille" />';
+                        $html .= '<div class="circleblack"></div>';
                     }
                 }
+
                 $html .= "</button></td> \n";
             }
             $html .= "\t \t </tr> \n";
         }
         $html .= "\t </table> \n";
+        $html .= '</body>';
         $html .= "</html>";
         echo $html; // affichage de tout l'html concaténé
     }
